f899ccc4d72b15f0553b5e93e96773c09bd980dd
Nathan Allen <nallenscott@me.com>
2022-09-28 19:03:40 -0400

    feat(build): update c client to 4.6.24

--- src/build.sh
+++ src/build.sh
@@ -17,7 +17,7 @@
 
 export CLIENTREPO_3X=${PWD}/../aerospike-client-c
 
-export AEROSPIKE_C_VERSION=${AEROSPIKE_C_CLIENT:-4.6.9}
+export AEROSPIKE_C_VERSION=${AEROSPIKE_C_CLIENT:-4.6.24}
 export DOWNLOAD_C_CLIENT=${DOWNLOAD_C_CLIENT:-1}
 export LUA_USRPATH=${LUA_USRPATH:-/usr/local/aerospike/usr-lua}
 
